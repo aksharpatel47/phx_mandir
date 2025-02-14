0a5f7fe7b8a5457d60f750afa7b947a656c687fe
JeenalAPatel <jspatel1993@gmail.com>
2025-02-13 20:03:11 -0700

    smaller registration form

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -4,11 +4,152 @@ export default function Page() {
   return (
     <>
       <Events
+        title={"Mahashivaratri"}
+        time={"Wednesday, February 26, 2025 - 6:30 PM"}
+        paragraph={`cordially invites you, your family and friends to celebrate MAHASHIVARATRI
+followed by Mahaprasad.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/Mahashivratri_thumbnail.png"
+        }
+      />
+      <Events
+        title={"Fuldol - Bhagatji Maharaj Janma Jayanti"}
+        time={"Saturday, March 08, 2025 - 4 PM"}
+        paragraph={`Join us as we celebrate the birth anniversary of Bhagatji Maharaj, the second successor of Bhagwan Swaminarayan. We are also celebrating Fuldol or Pushpadolotsav on the arrival of spring. Traditionally a murti of Bhagwan is placed on a swing beautifully decorated with flowers. Devotees both serve and offer their sincere devotion to Bhagwan by gently setting the swing in motion, experiencing His darshan in this unique way. The changing season, new blooms, and vibrant colors of spring are celebrated by Hindus across the world.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/Fuldol_thumbnail.jpg"
+        }
+      />
+      <Events
+        title={"Shri Swaminarayan Jayanti Mahila Celebration"}
+        time={"Saturday, April 05, 2025 - 4 PM"}
+        paragraph={`Celebrating the birth anniversaries of Bhagwan Swaminarayan and Bhagwan Ram, the women of BAPS will organize a cultural program featuring kirtans, spiritual discourses, traditional dances and dramas depicting the life of Bhagwan Swaminarayan and Bhagwan Ram.
+
+A special program organized by Women for Women.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/Swaminarayan_Jayanti_mahila_thumbnail.jpg"
+        }
+      />
+      <Events
+        title={"Shri Swaminarayan Jayanti & Shri Ram Navmi"}
+        time={"Saturday, April 12, 2025 - 4 PM"}
+        paragraph={`This celebration honors the births of Bhagwan Swaminarayan, whose philosophy is the core of the BAPS Swaminarayan Sanstha, and Bhagwan Ram, whose life and ideals are the inspiration of the Ramayana. The teachings and messages of Bhagwan Swaminarayan’s and Bhagwan Ram’s lives will be presented through spiritual discourses and a youth-led cultural program. Join us for a cultural program performed by the youth of BAPS, as well as spiritual discourses by revered Swamis.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/Swaminarayan_Jayanti_thumbnail.jpg"
+        }
+      />
+      <Events
+        title={"Yogi Javanti"}
+        time={"Saturday, May 24, 2025 - 4 PM"}
+        paragraph={`Yogiji Maharaj, the fourth spiritual successor of Bhagwan Swaminarayan, was born in Dhari in 1892. He seemed like a simple sadhu but his inner simplicity was vast. His faith and spirituality led the philosophy of Akshar Purushottam beyond India, to various corners of the globe. Yogiji Maharaj is the visionary, inspirer, and initiator of the organization’s weekly children and youth activities. His positive attitude and engaging smile transformed the hearts of thousands of people. Please join the celebration of Yogiji Maharaj Janma Jayanti.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/Yogi_Jayanti_thumbnail.jpg"
+        }
+      />
+      <Events
+        title={"Guru Purnima"}
+        time={"Saturday, July 12, 2025 - 4 PM"}
+        paragraph={`Guru Purnima (Vyas Purnima), is celebrated on Ashadh Sud Poonam, the day of 
+        the full moon in the month of Ashadh. On this day, Hindus remember Ved Vyas, the eternal 
+        guru of Hindu Sanatan Dharma, as he classified the 4 Vedas (Rig, Yajur, Sama, and Atharva) 
+        and wrote the Mahabharata and 18 Puranas. Guru, a Sanskrit word, comes from the root words 
+        ‘gu’ meaning darkness or ignorance, and ‘ru’ meaning remover of that darkness. A guru is 
+        one who removes our darkness in the form of ignorance, helps us overcome maya, and guides 
+        us on the path to realizing Bhagwan. Join us in offering guru bhakti, our devotion, 
+        and earnest prayers, to His Holiness Mahant Swami Maharaj, with bhajans, video
+         presentations and spiritual discourses by swamis and youth.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/Guru_Punam_thumbnail.jpg"
+        }
+      />
+      <Events
+        title={"Janmashtami Celebration"}
+        time={"Saturday, August 16, 2025 - 4 PM"}
+        paragraph={`The month of Shravan in the Hindu calendar is considered among 
+        the holiest of the year. It brings many different festivals, one of the most 
+        significant is known as Janmashtami, 
+        or the birth of Bhagwan Shri Krishna. Please join us to celebrate Janmashtami.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/Janmashtmi_thumbnail.jpg"
+        }
+      />
+      <Events
+        title={"Mahant Swami Maharaj’s Janma Jayanti"}
+        time={"Saturday, September 13, 2025 - 4 PM"}
+        paragraph={`His Holiness Mahant Swami Maharaj is the sixth spiritual 
+        successor of Bhagwan Swaminarayan and spiritual leader of the BAPS 
+        Swaminarayan Sanstha. He lives a devout, humble and service-focused life. 
+        He leads by example and inspires people to progress on the spiritual path. 
+        His virtuous lifestyle and profound devotion to Bhagwan Swaminarayan and his gurus 
+        are ideals towards which devotees strive. 
+        Join us to celebrate His Holiness Mahant Swami Maharaj’s Janma Jayanti.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/MSM__thumbnail.jpg"
+        }
+      />
+      <Events
+        title={"Mahant Swami Maharaj Janma Jayanti - Mahila Celebration"}
+        time={"Saturday, September 20, 2025 - 4 PM"}
+        paragraph={`Celebrating the birth anniversary of His Holiness 
+        Mahant Swami Maharaj, the women of BAPS will organize a unique cultural 
+        program with the theme exploring 
+        the moments from their guru’s life that serve as teachable examples.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/MSM_mahila_thumbnail.jpg"
+        }
+      />
+      <Events
+        title={"Sharad Purnima - Akshar Jayanti"}
+        time={"Saturday, October 04, 2025 - 4 PM"}
+        paragraph={`Sharad Purnima is celebrated on the full moon of the Hindu lunar month Ashwin, and honors the significant role of the moon in Hinduism. It is the birth anniversary of Aksharbrahma Gunatitanand Swami, the first successor of Bhagwan Swaminarayan, and is a spiritually significant day for BAPS Swaminarayan Sanstha. 
+        Join us in a celebration that includes special discourses and bhajans.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/Sharad_Punam_thumbnail.jpg"
+        }
+      />
+      <Events
+        title={"Bhakti Parva - Ladies Garba (Navaratri)"}
+        time={"Saturday, October 11, 2025"}
+        paragraph={`Join us for a Garba program organized by women, for women in celebration of Navaratri.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/Bhaktiparva_thumbnail.jpg"
+        }
+      />
+      <Events
+        title={"Diwali & Sharda (Chopda) Pujan"}
+        time={"Monday, October 20, 2025 - 6 PM - 9 PM"}
+        paragraph={`Diwali – one of the most auspicious days in the Hindu calendar will be celebrated with great devotion and splendor. On this day, business-owners close existing account books and open new ones in preparation for the year ahead. Devotees also take stock of their spiritual relationship with Bhagwan, and consider any progress or regress they have made in the year past. The ceremony, known as Chopda Pujan, will take place in the evening. Hundreds of new account books – in a variety of paper and electronic forms – are sanctified through the showering of 
+        flowers petals and rice grains amid the chanting of Vedic hymns and prayers.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/Chopda_Pujan_013.jpg"
+        }
+      />
+      <Events
         title={"Family Annakut Darshan"}
-        time={"Saturday, November 02, 2024 - 12PM - 7PM"}
-        paragraph={`The day following Diwali is traditionally celebrated with the Annakut – literally meaning, a mountain of food. Every year thousands of vegetarian delicacies are offered in devotional gratitude for the past year and to seek blessings for the year ahead.  The Annakut offering includes snacks, sweets, pickles, spicy dishes, salads, fruit drinks and other items devotionally prepared by devotees. We cordially invite you for darshan of the Annakut with family and friends.`}
+        time={"Saturday, October 25, 2025 - 12 PM - 6 PM"}
+        paragraph={`The day following Diwali is traditionally celebrated with the Annakut – literally meaning, a mountain of food. Every year thousands of vegetarian delicacies are offered in devotional gratitude for the past year and to seek blessings for the year ahead.  The Annakut offering includes snacks, sweets, pickles, spicy dishes, salads, fruit drinks and other items devotionally prepared by devotees. 
+        We cordially invite you for darshan of the Annakut with family and friends.`}
         img={
           "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/02_PHX_BAPS_Annakut_2019.jpg"
+        }
+      />
+      <Events
+        title={"Pramukh Swami Maharaj's Birthday Celebration"}
+        time={"Saturday, November 29, 2025 - 4 PM"}
+        paragraph={`Pramukh Swami Maharaj is an inspiration for countless people around the globe. During the course of his lifetime, he has personally counseled countless people, having read and replied to over half a million letters from individuals of all walks of life. 
+        He inspired thousands to lead lives grounded in spiritually and selfless service.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/PSM_Janma_Jayanti_thumbnail.jpg"
+        }
+      />
+      <Events
+        title={"Pramukh Swami Maharaj Jayanti - Mahila (Ladies) Celebration"}
+        time={"Saturday, December 06, 2025 - 4 PM"}
+        paragraph={`Pramukh Swami Maharaj is an inspiration for countless people around the globe. During the course of his lifetime, he has personally counseled countless people, having read and replied to over half a million letters from individuals of all walks of life. He inspired thousands to lead lives grounded in spiritually and selfless service. The women of BAPS has organized a unique cultural program to celebrate Pramukh Swami Maharaj’s life, legacy, and teachings.
+
+Join us in a unique cultural program organized by women, for women.`}
+        img={
+          "https://www.baps.org/Data/Sites/1/Media/LocationImages/upcomingevents/WebImages/PSM__Janma_Jayanti_mahila_thumbnail.jpg"
         }
       />
     </>
